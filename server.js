5c50a8788d1651aa4ea042f61eacba9d844d291a
Matt Boggie <mboggie@mac.com>
2014-04-08 17:38:04 -0400

    v0.1: consistently gets gtfs data, and returns array of vehicles with extended metadata, including stop details and translated datetime

--- server.js
+++ server.js
@@ -16,35 +16,36 @@ var STOPFILE    = "./stops.txt";
 var builder = ProtoBuf.loadProtoFile("lib/protos/nyct-subway.proto");
 var decoder = builder.build("transit_realtime").FeedMessage;
 
-var current_status_text = ["INCOMING AT", "STOPPED AT", "IN TRANSIT TO"];
+var current_status_enum = ["INCOMING AT", "STOPPED AT", "IN TRANSIT TO"];
+
+//build a lookup array of objects for the station lookups
 var csvconverter = new Converter();
 var stopLookup = {};
 csvconverter.on("end_parsed", function(jsonobj){
-  //build a simpler lookup object array here
-  //console.log(jsonobj);
   ray = jsonobj.csvRows;
   for (var i = ray.length - 1; i >= 0; i--) {
-    stopLookup[ray.stop_id] = {stop_name:ray.stop_name, stop_lat:ray.stop_lat, stop_lon:ray.stop_lon};
+    stopLookup[ray[i].stop_id] = {id:ray[i].stop_id, name:ray[i].stop_name, lat:ray[i].stop_lat, lon:ray[i].stop_lon};
   };
 
 });
 
 csvconverter.from(STOPFILE);
 
+
+//modify the JSON we get from parsing GTFS to include only what we need / reformat some data
 function squishjson(msg) {
 	var entities = msg.entity;
   var returnarray = [];
   for (var i=0; i<entities.length; i++) {
     if (entities[i].vehicle != null){
       var tempdate = new Date(entities[i].vehicle.timestamp.low * 1000);
-      entities[i].vehicle.timestamp = tempdate.toISOString();
-      entities[i].vehicle.current_status = current_status_text[entities[i].vehicle.current_status];
-      //FIGURE THIS BIT OUT
-      //console.log(entities[i].vehicle.stop_id);
+      entities[i].vehicle.datetime = tempdate.toISOString();
+      entities[i].vehicle.current_status_text = current_status_enum[entities[i].vehicle.current_status];
+      entities[i].vehicle.stop_details = stopLookup[entities[i].vehicle.stop_id];
       returnarray.push(entities[i].vehicle);
     }
   }
-	return returnarray.slice(0,4);
+	return returnarray; //.slice(0,4);
 }
 
 app.get('/', function(req, out) {
